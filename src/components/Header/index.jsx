9e532f31923e22bb24c9975176bf3af39a129c80
Tiago Bertoldo <berto.hlb2@gmail.com>
2022-11-14 18:26:37 +0100

    header resposive + desktop

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,56 @@
+import "../StyleGlobal/index.scss"
+import "./Style/desktop.scss"
+import "./Style/resposive.scss"
+import bresil from "./assets/img/bresil.png"
+import france from "./assets/img/france.png"
+import usa from "./assets/img/usa.png"
+import { Link } from "react-router-dom"
+import { FiAlignJustify } from "react-icons/fi";
+
 export default function Header(){
+    const headlResposive =() => {
+        let btnUl = document.querySelector(".list-ul")
+        if(btnUl !== null) {
+            btnUl.classList.toggle("resposive-effect")
+        }
+    }
     return (
         <>
-            <h2>TESTE</h2>
+            <div className="container-header">
+                <div className="container-header__box">
+                    <div className="container-header__left">
+                        <div className="resposive-btn" onClick={() => headlResposive()}><FiAlignJustify/></div>
+                        <ul className="list-ul">
+                            <li>
+                                <Link className="btn-link" to="/"> Home</Link>
+                            </li>
+                            <li>
+                                <Link className="btn-link" to="/"> MyIntro</Link>
+                            </li>
+                            <li>
+                                <Link className="btn-link" to="/"> Portifolio</Link>
+                            </li>
+                            <li>
+                                <Link className="btn-link linked" to="/"> Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="container-header__right">
+                        <ul>
+                            <li>
+                                <Link className="flags-idioma"><img src={bresil} alt="flag bresil" /></Link>
+                            </li>
+                            <li>
+                                <Link className="flags-idioma"><img src={france} alt="flag france" /></Link>
+                            </li>
+                            <li>
+                            <Link className="flags-idioma"><img src={usa} alt="flag usa" /></Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                
+            </div>
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
+import Home from './components/Home';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
+      
+      <Router>
+        <Header />
+        <Routes>
+            <Route path='/' element={< Home/>}>
+            <Route path='/myintro' element={<Home/>}/>
+            </Route>
+        </Routes>
+      </Router>
+    
+  
   </React.StrictMode>
 );
